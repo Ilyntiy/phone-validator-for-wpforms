bae9c87ea168d6ce9957bda61cade2207a87e938
Ilyntiy <xryndelson@gmail.com>
2026-02-11 15:57:53 +0300

    Update version constant to 1.0.1

--- phone-validator-for-wpforms.php
+++ phone-validator-for-wpforms.php
@@ -20,7 +20,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Plugin Constants
-define('WPFPV_VERSION', '1.0.0');
+define('WPFPV_VERSION', '1.0.1');
 define('WPFPV_PLUGIN_FILE', __FILE__);
 define('WPFPV_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('WPFPV_PLUGIN_URL', plugin_dir_url(__FILE__));
